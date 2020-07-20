79ac265399b6ff91dbf9250534d1542773542724
Hyemin <Hyemin@DESKTOP-MUHMQ80>
2020-07-20 14:02:26 +0900

    Calculator

--- JavaProject1/src/variable/Calculator.java
+++ JavaProject1/src/variable/Calculator.java
@@ -4,6 +4,17 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num1 = 11, num2 = 22;
+		
+		int sum = num1 + num2;
+		int sub = num1 - num2;
+		int mul = num1 * num2;
+		double div = (double)num1 / (double)num2;
+		
+		System.out.println(num1 + " + " + num2 + " = " + sum + "\n");
+		System.out.println(num1 + " - " + num2 + " = " + sub + "\n");
+		System.out.println(num1 + " * " + num2 + " = " + mul + "\n");
+		System.out.println(num1 + " / " + num2 + " = " + div + "\n");
 		
 	}
 
--- JavaProject1/src/variable/Half.java
+++ JavaProject1/src/variable/Half.java
@@ -7,14 +7,16 @@ public class Half {
 		double num1 = 2.7;
 		double num2 = 3.4;
 		
-		int half1;
+		int half1 = (int)(num1 + 0.5);
 		
+		/*
 		if ((num2%1) >= 0.5) {
-			half1= (int)num2 + 1;
+			half1 = (int)num2 + 1;
 		}
 		else {
 			half1 = (int)num2;
 		}
+		*/
 		
 		System.out.println(half1);
 	}
